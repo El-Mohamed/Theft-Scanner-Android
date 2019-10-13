f9ce037c95ca11cfe667579a795f152dba32683d
Mohamed <elmoufid.mohamed@outlook.com>
2019-10-13 18:05:53 +0200

    Add Button Handler
    Click on button -> Go back to MainActivity + Toast Message

--- app/src/main/java/com/example/theftscanner/Form.java
+++ app/src/main/java/com/example/theftscanner/Form.java
@@ -2,13 +2,33 @@ package com.example.theftscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Form extends AppCompatActivity {
+
+    Button SendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+
+        SendButton = findViewById(R.id.send_button);
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Form.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(Form.this, R.string.toast_message, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
