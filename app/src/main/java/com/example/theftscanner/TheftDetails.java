aa586e072fb186e265989515177e07a6c4f5ee25
Mohamed <elmoufid.mohamed@outlook.com>
2019-11-07 16:51:56 +0100

    Add DatabaseReader + KeyboardHelper
    Add databasereader class + keyboardhelper class
    + Refactor theft details activity

--- app/src/main/java/com/example/theftscanner/TheftDetails.java
+++ app/src/main/java/com/example/theftscanner/TheftDetails.java
@@ -1,21 +1,13 @@
 package com.example.theftscanner;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,69 +17,38 @@ public class TheftDetails extends AppCompatActivity {
     RecyclerView recyclerView;
     Adapter adapter;
 
-    DatabaseReference mDatabaseRef;
+    EditText mSearchText;
+    Button mSearchButton;
+
+    DatabaseReader databaseReader;
     List<Theft> mThefts;
-    Theft myTheft;
-    EditText mCity;
-    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theft_details);
-        mButton = findViewById(R.id.filter_button_details);
-        mCity = findViewById(R.id.preferred_city_details);
+        mSearchButton = findViewById(R.id.search_button_details);
+        mSearchText = findViewById(R.id.search_text_details);
+
         recyclerView = findViewById(R.id.recyclerview_details);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         mThefts = new ArrayList<>();
+        databaseReader = new DatabaseReader();
 
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Thefts");
-
-
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-                mDatabaseRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        String City = mCity.getText().toString().toLowerCase();
-
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-                            String city = snapshot.child("city").getValue().toString().toLowerCase();
-                            if (city.equals(City)) {
-
-                                myTheft = snapshot.getValue(Theft.class);
-                                mThefts.add(myTheft);
-                                String type = snapshot.child("type").getValue().toString();
-                                Log.d("testje", type);
-
-
-                            }
-
-
-                        }
-
-                        adapter = new Adapter(TheftDetails.this, mThefts);
-                        recyclerView.setAdapter(adapter);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
+                String City = mSearchText.getText().toString();
+                mThefts = databaseReader.GetAllThefts(City);
+                KeyboardHelper.hideKeyboard(TheftDetails.this);
+                adapter = new Adapter(TheftDetails.this, mThefts);
+                recyclerView.setAdapter(adapter);
 
             }
         });
 
-
     }
-
 
 }
