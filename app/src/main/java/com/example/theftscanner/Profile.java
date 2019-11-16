f01ce6ef194963287bcd5c112472b664c63c22dc
Mohamed <elmoufid.mohamed@outlook.com>
2019-11-16 20:50:30 +0100

    Profile Refactor

--- app/src/main/java/com/example/theftscanner/Profile.java
+++ app/src/main/java/com/example/theftscanner/Profile.java
@@ -13,27 +13,50 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Profile extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-
-    TextView mCurrentAccount;
-    TextView mCurrentID;
-    Button mButton;
+    private FirebaseAuth mFirebaseAuth;
+    FirebaseUser currentUser;
+    TextView mCurrentAccount, mCurrentID;
+    Button mLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        currentUser = mFirebaseAuth.getCurrentUser();
 
         mCurrentAccount = findViewById(R.id.current_mail);
         mCurrentID = findViewById(R.id.email_id);
-        mButton = findViewById(R.id.logout_login);
+        mLogoutButton = findViewById(R.id.logout_button);
 
-        if (user != null) {
-            String CurrentEmail = user.getEmail();
-            String CurrentEmailID = user.getUid();
+        checkCurrentUser();
+
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+
+        });
+
+    }
+
+    private void logoutUser() {
+
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(Profile.this, Dashboard.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void checkCurrentUser() {
+
+        if (currentUser != null) {
+            String CurrentEmail = currentUser.getEmail();
+            String CurrentEmailID = currentUser.getUid();
             mCurrentAccount.setText(CurrentEmail);
             mCurrentID.setText(CurrentEmailID);
         } else {
@@ -41,22 +64,6 @@ public class Profile extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (mAuth.getCurrentUser() != null) {
-                    FirebaseAuth.getInstance().signOut();
-                    Intent intent = new Intent(Profile.this, Dashboard.class);
-                    startActivity(intent);
-                    finish();
-                }
-
-            }
-
-        });
-
     }
 
 }
